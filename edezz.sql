c2a84f14f3eff22e97309bdab90934bbe6a93713
DDibyajyot <84494384+DDibyajyot@users.noreply.github.com>
2021-06-27 08:00:58 +0100

    Add files via upload
    sql part of the api

---
+++ edezz.sql
@@ -0,0 +1,60 @@
+create table Books2 (
+Name char(30),
+Grade integer,
+Subject char(20));
+
+
+
+INSERT INTO Books2 VALUES('NCERT Maths', 6, 'Mathematics');
+INSERT INTO Books2 VALUES('CK-12 Interactive Middle School Math 6', 6,'Mathematics');
+INSERT INTO Books2 VALUES('Literacy Foundation Math L-6', 6, 'Mathematics');
+INSERT INTO Books2 VALUES('6th Grade Math Students Materials',6 ,'Mathematics');
+INSERT INTO Books2 VALUES('Grade 6 Khan Academy Maths', 6,'Mathematics');
+
+INSERT INTO Books2 VALUES('CK-12 Earth Science for Middle School', 6,'Science');
+INSERT INTO Books2 VALUES('NCERT Science Textbook',6,'Science');
+INSERT INTO Books2 VALUES('Thunderbolt Natural Sciences and Technology',6,'Science');
+INSERT INTO Books2 VALUES('Flexbook 6th grade Science', 6, 'Science');
+
+INSERT INTO Books2 VALUES('NCERT Maths', 7, 'Mathematics');
+INSERT INTO Books2 VALUES('CK-12 Interactive Middle School Math 7', 7,'Mathematics');
+INSERT INTO Books2 VALUES('Grade 7 Khan Academy Maths', 7,'Mathematics');
+INSERT INTO Books2 VALUES('Literacy Foundation Math L-7 Geometry', 7, 'Mathematics');
+INSERT INTO Books2 VALUES('CAPS Learner Book Inzalo Math', 7, 'Mathematics');
+
+INSERT INTO Books2 VALUES('NCERT Science', 7, 'Science');
+INSERT INTO Books2 VALUES('CK-12 Physical Science for Middle School', 7, 'Science');
+INSERT INTO Books2 VALUES('FreeKidsBooks 7th Grade Science', 7, 'Science');
+INSERT INTO Books2 VALUES('7th Grade Science Compilation', 7, 'Science');
+
+INSERT INTO Books2 VALUES('NCERT Maths', 8, 'Mathematics');
+INSERT INTO Books2 VALUES('CAPS Learner Book Inzalo Math', 8, 'Mathematics');
+INSERT INTO Books2 VALUES('CK-12 Interactive Middle School Math 8', 8,'Mathematics');
+INSERT INTO Books2 VALUES('UEN 8th Grade Mathematical Foundations', 8, 'Mathematics');
+INSERT INTO Books2 VALUES('Literacy Foundation Math L-8 Geometry', 8, 'Mathematics');
+INSERT INTO Books2 VALUES('Math Grade8 Compilation of Chapters', 8, 'Mathematics');
+
+INSERT INTO Books2 VALUES('NCERT Science', 8, 'Science');
+INSERT INTO Books2 VALUES('CK-12 Physical Science for Middle School', 8,'Science');
+INSERT INTO Books2 VALUES('8th Grade Science Compilation', 8, 'Science');
+INSERT INTO Books2 VALUES('FreeKidsBooks 8th Grade Science', 8, 'Science');
+
+INSERT INTO Books2 VALUES('NCERT Maths', 9, 'Mathematics');
+INSERT INTO Books2 VALUES('CK-12 PreCalculus Concepts', 9, 'Mathematics');
+INSERT INTO Books2 VALUES('E-media for Mathematics Grade9', 9, 'Mathematics');
+INSERT INTO Books2 VALUES('Siyavula Mathematics Grade9 Resources', 9, 'Mathematics');
+
+INSERT INTO Books2 VALUES('NCERT Science', 9, 'Science');
+INSERT INTO Books2 VALUES('CK-12 Interactive Physics for High School', 9, 'Science');
+INSERT INTO Books2 VALUES('CK-12 Chemistry for High School', 9, 'Science');
+INSERT INTO Books2 VALUES('Grade 9 Epub Science Compilation of Textbooks', 9, 'Science');
+
+INSERT INTO Books2 VALUES('NCERT Maths', 10, 'Mathematics');
+INSERT INTO Books2 VALUES('AWM Mathematics Textbook Trigonometry', 10, 'Mathematics');
+INSERT INTO Books2 VALUES('Maths Epub for Grade10 Textbooks', 10, 'Mathematics');
+INSERT INTO Books2 VALUES('Secondary Mathematics: Integrated Pathway CCSS', 10, 'Mathematics');
+
+INSERT INTO Books2 VALUES('NCERT Science', 10, 'Science');
+INSERT INTO Books2 VALUES('Life Sciences Grade 10', 10, 'Science');
+INSERT INTO Books2 VALUES('Grade10 Physical Science', 10, 'Science');
+INSERT INTO Books2 VALUES('CK-12 Physical Science for High School', 10, 'Science');
